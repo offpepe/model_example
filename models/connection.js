035fdb18f3c241fc091293d560e0b67ad7c0e00a
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-26 22:23:18 -0400

    fix: connection function

--- models/connection.js
+++ models/connection.js
@@ -4,7 +4,7 @@ const MONGO_URL = 'mongodb://127.0.0.1:27017';
 const connection = () => {
     return MongoClient
         .connect(MONGO_URL, {
-            urlNewParser: true,
+            useNewUrlParser: true,
             useUnifiedTopology: true,
         }).then((conn) => conn.db('model_example'))
         .catch((err) => {
